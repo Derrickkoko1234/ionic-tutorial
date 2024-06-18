d67e2d6142de51e6ce7fd32c970ec7a1de2c6256
Derrick Koko <kokoderrick3@gmail.com>
2024-06-18 15:43:58 +0100

    refactor: Update router configuration and remove HomePage.vue

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-title>Memories</ion-title>
+    </ion-header>
+    <ion-content>
+      <h1>Memories</h1>
+    </ion-content>
+  </ion-page>
+</template>
+<script>
+import {IonPage, IonHeader, IonTitle, IonContent} from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent
+  }
+};
+</script>
